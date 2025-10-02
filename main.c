2bf157e12b4945d5930bc63b3d5e4cdc2ee1bbbb
Francesco Cerbai <1ecerbaifrancesco@gmail.com>
2025-10-02 17:10:18 +0200

    fixed main.c

--- main.c
+++ main.c
@@ -11,15 +11,18 @@ int main(void) {
     char *dup;
     char *dup2;
     ssize_t w_ret, r_ret;
-    char buf[20];
+    char buf[50];
     int cmp;
 
+    printf("===== MANDATORY TESTS =====\n");
     // ft_strlen
+    printf("-------- ft_strlen --------\n");
     printf("ft_strlen: %lu\n", ft_strlen(src));
     printf("strlen: %lu\n", strlen(src));
     printf("\n");
 
     // ft_strcpy
+    printf("-------- ft_strcpy --------\n");
     ft_strcpy(dest, src);
     strcpy(dest2, src);
     printf("ft_strcpy: %s", dest);
@@ -27,6 +30,7 @@ int main(void) {
     printf("\n");
 
     // ft_strcmp
+    printf("-------- ft_strcmp --------\n");
     cmp = ft_strcmp(src, dest);
     printf("ft_strcmp: %d\n", cmp);
     cmp = ft_strcmp(src, "Hello");
@@ -38,6 +42,7 @@ int main(void) {
     printf("\n");
 
     // ft_strdup
+    printf("-------- ft_strdup --------\n");
     dup = ft_strdup(src);
     printf("ft_strdup: %s", dup);
     free(dup);
@@ -47,6 +52,7 @@ int main(void) {
     printf("\n");
 
     // ft_write
+    printf("-------- ft_write ---------\n");
     write(1, "ft_write: ", 11);
     w_ret = ft_write(1, src, ft_strlen(src));
     printf("Return value: %ld\n", w_ret);
@@ -56,21 +62,25 @@ int main(void) {
     printf("\n");
 
     // ft_read
-    write(1, "ft_read: Type something... ", 27);
+    printf("--------- ft_read ---------\n");
+    write(1, "Type something\nft_read: ", 24);
     r_ret = ft_read(0, buf, sizeof(buf)-1);
     if (r_ret >= 0) {
         buf[r_ret] = '\0';
-        printf("ft_read: %s", buf);
+        printf("Buffer: %sReturn value: %ld\n", buf, r_ret);
     } else {
         perror("ft_read");
     }
-    write(1, "read: Type something... ", 25);
+    write(1, "Type something\nread: ", 21);
     r_ret = read(0, buf, sizeof(buf)-1);
     if (r_ret >= 0) {
         buf[r_ret] = '\0';
-        printf("read: %s", buf);
+        printf("Buffer: %sReturn value: %ld\n", buf, r_ret);
     } else {
         perror("read");
     }
+
+    printf("\n===== BONUS TESTS =====\n");
+
     return 0;
 }
