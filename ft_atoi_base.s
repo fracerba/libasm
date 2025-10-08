6950ea554dcf6baffc770ee938da69a0de809443
fracerba <1ecerbaifrancesco@gmail.com>
2025-10-08 13:10:51 +0200

    registers

--- ft_atoi_base.s
+++ ft_atoi_base.s
@@ -2,8 +2,10 @@ global ft_atoi_base
 
 section .text
 ft_atoi_base:
-    mov     rax, 0          ; rax = return value
-    mov     rcx, 0          ; rcx = sign 
-    mov     rdx, 0          ; rdx = base value
-    mov     r8, 0           ; r8 = str counter
-    mov     r9, 0           ; r9 = base counter
+    mov     rax, 0          ; rax = return value, buffer
+    mov     rcx, 0          ; rcx = src counter
+    mov     rdx, 0          ; rdx = base counter
+    mov     r8, 0           ; r8 = number
+    mov     r9, 0           ; r9 = sign
+    mov     r10, 0          ; r10 = base
+    mov     r11, 0          ; r11 = digit
